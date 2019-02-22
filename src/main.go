f60df93979df62571fad1b55abf18460b85dfcd9
nalwayv <nalwayv@googlemail.com>
2019-02-22 21:01:01 +0000

    update

--- src/main.go
+++ src/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"./game"
-	"./gologger"
 	"github.com/veandco/go-sdl2/sdl"
 )
 
@@ -13,32 +12,18 @@ const (
 	WINDOW_HEIGHT int32 = 480
 )
 
-// Globals ...
-var (
-	golog = gologger.GetInstance("src/gologger/golog.log")
-)
-
 //------------------------------------
 
-// checkError ... check for errors
-func checkError(err error) {
-	if err != nil {
-		golog.Fatalln(err)
-	}
-}
-
 func run() {
-	var err error
 	ggame := &game.Game{}
 	defer ggame.Clean()
 
-	err = ggame.Init("sdl",
+	ggame.Init("sdl",
 		sdl.WINDOWPOS_UNDEFINED,
 		sdl.WINDOWPOS_UNDEFINED,
 		WINDOW_WIDTH,
 		WINDOW_HEIGHT,
 		false)
-	checkError(err)
 
 	for ggame.Running {
 		ggame.HandleEvents()
@@ -47,7 +32,6 @@ func run() {
 	}
 }
 
-//------------------------------------
 func main() {
 	run()
 }
